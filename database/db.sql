a645c23894f4bf0e0853c456a64949bbba2e727e
= <=>
2022-03-13 15:23:43 -0500

    Creación de base de datos y relaciones

---
+++ database/db.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE exval;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS priorities;
+DROP TABLE IF EXISTS interests;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS capitals;
+
+
+CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        user VARCHAR(10) NOT NULL UNIQUE,
+        pass VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE types(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(15) NOT NULL
+);
+CREATE TABLE stocks(
+        code CHAR(3) PRIMARY KEY,
+        name VARCHAR(15) NOT NULL UNIQUE,
+        value FLOAT NOT NULL
+);
+CREATE TABLE priorities(
+        id SERIAL PRIMARY KEY,
+        stk_code CHAR(3) REFERENCES stocks(code),
+        id_user INT REFERENCES users(id)
+);
+
+
+CREATE TABLE insterests(
+        type INT REFERENCES types(id),
+        stk_code CHAR(3) REFERENCES stocks(code),
+        percentage DECIMAL(5,2) NOT NULL,
+        PRIMARY KEY(type, stk_code)
+);
+CREATE TABLE transactions(
+        id INT PRIMARY KEY,
+        id_user INT REFERENCES users(id),
+        id_type INT REFERENCES types(id),
+        stk_from CHAR(3) REFERENCES stocks(code),
+        stk_to CHAR(3) REFERENCES stocks(code),
+        amount INT NOT NULL,
+        date DATETIME NOT NULL,
+        interest INT 
+);
+
+CREATE TABLE capitals(
+        id INT PRIMARY KEY,
+        stk_code CHAR(3) REFERENCES stocks(code),
+        id_user INT REFERENCES users(id),
+        amount INT CHECK (amount >= 0) NOT NULL
+);
