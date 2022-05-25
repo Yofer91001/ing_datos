06451029b23d8c89f0a1785032ab55da97addd04
valentinahernandezq <100850859+valentinahernandezq@users.noreply.github.com>
2022-05-25 17:28:41 -0500

    Update monedas_mas_movimiento.py

--- database/monedas_mas_movimiento.py
+++ database/monedas_mas_movimiento.py
@@ -1,3 +1,5 @@
+##El link para visualizar la grafica aparece en consola al correr el codigo.
+
 import psycopg2
 import pandas as pd
 import plotly.express as px
@@ -26,3 +28,4 @@ app.layout = html.Div([
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
     
+  
